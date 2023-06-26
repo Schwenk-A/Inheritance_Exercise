71adf55911de989c1afabb9f2be6499ac20b240b
Schwenk-A <100094451+Schwenk-A@users.noreply.github.com>
2023-06-26 08:18:20 -0500

    Created base logic for exercise

---
+++ EmployeeExercise/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeExercise
+{
+    public class Employee
+    {
+        public string name { get; set; }
+        public string department { get; set; }
+        public double salary { get; set; }
+        public DateTime start_date { get; set; }
+
+        public double GetRaise()
+        {
+            return salary * 0.05;
+        }
+
+        public void ChangeDepartment(string new_department)
+        {
+            department = new_department;
+        }
+
+        public double GetBonus()
+        {
+            return salary * 0.10;
+        }
+    }
+}
+
